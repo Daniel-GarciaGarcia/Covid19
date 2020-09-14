406c16fba586b6717284465dd73f038779eb201f
Daniel-GarciaGarcia <kasiya93@gmail.com>
2020-09-14 19:38:47 +0200

    lunesdemuerte

--- inputs.py
+++ inputs.py
@@ -3,20 +3,22 @@ import os
 import requests
 import json
 from dotenv import load_dotenv
+import requests_oauthlib
 load_dotenv()
 
 
 def get_auth():
+
     load_dotenv()
-    ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-    ACCESS_SECRET = os.getenv("ACCESS_SECRET")
-    CONSUMER_KEY = os.getenv("CONSUMER_KEY")
-    CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
-    my_auth = requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN, ACCESS_SECRET)
+    CONSUMER_KEY=os.getenv("CONSUMER_KEY")
+    CONSUMER_SECRET=os.getenv("CONSUMER_SECRET")
+    ACCESS_TOKEN=os.getenv("ACCESS_TOKEN")
+    ACCESS_SECRET=os.getenv("ACCESS_SECRET")
+    my_auth=requests_oauthlib.OAuth1(CONSUMER_KEY, CONSUMER_SECRET,ACCESS_TOKEN, ACCESS_SECRET)
     return my_auth
 
 
-def get_tweet(endpoint, apiKey=os.getenv('CONSUMER_KEY'), query_params={}):
+'''def get_tweet(endpoint, apiKey=os.getenv('CONSUMER_KEY'), query_params={}):
     tweet_fields = "tweet.fields=lang,author_id"
 
     ids = "ids=1303677963200798720,1255542774432063488"
@@ -29,16 +31,7 @@ def get_tweet(endpoint, apiKey=os.getenv('CONSUMER_KEY'), query_params={}):
 
     if res.status_code != 200:
         raise ValueError(f"Invalid response: {data['message']} .")
-    return data
-
-'''def Gather_Tweets():
-    friends=api.friends_ids()
-    for friend in friends:
-        new_tweets= api.user_timeline(api.get_user(friend).screen_name, count=20, page=3, tweet_mode='extended')
-        tweets=[tweet.full_text.upper() for tweet in new_tweet]
-        all.append(tweets)
-        return all
-print(Gather_Tweets())'''
+    return data'''
 
 def Split_Tweets():
     all_tweets=Gather_Tweets()
@@ -59,8 +52,14 @@ def Sort_Covid():
                 cov.append(word)
     return cov
 
-    def missing_value_of_data(df):
+def missing_value_of_data(df):
     total=df.isnull().sum().sort_values(ascending=False)
     percentage=round(total/df.shape[0]*100,2)
     return pd.concat([total,percentage],axis=1,keys=['Total','Percentage'])
 
+def get_tweets(user):
+    my_auth = get_auth()
+    url = f'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name={user}&count=100&tweet_mode=extended'
+    response = requests.get(url, auth=my_auth, stream=True)
+    print(url, response)
+    return response.json()
